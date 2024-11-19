b57a929b06c6232800fe02926c97efb85b4f2b23
Gloriaamandagii <glorriiaamandagii@gmail.com>
2024-11-19 13:47:38 +0800

    insert service data into firebase

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,17 +1,31 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 const Service = () => {
+  const [service, setService] = useState({});
+
+  useEffect(() => {
+    const db = getDatabase();
+    const serviceRef = ref(db, "service");
+    onValue(serviceRef, (snapshot) => {
+      const data = snapshot.val();
+      setService(data);
+    });
+  }, []);
+
   return (
     <section id="services" className="main-container">
-      <h1 className="section-title services-title">Interested In</h1>
+      <h1 className="section-title services-title">{service.title}</h1>
       <div className="grid-3">
         <div className="service">
-          <h4>Web Development</h4>
+          <h4>{service.interest1}</h4>
         </div>
 
         <div className="service">
-          <h4>Application Development</h4>
+          <h4>{service.interest2}</h4>
         </div>
         <div className="service">
-          <h4>Front-End Development</h4>
+          <h4>{service.interest3}</h4>
         </div>
       </div>
     </section>
